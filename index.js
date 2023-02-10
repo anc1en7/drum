e66148c4f8b90565f83fb2cb37f2c2e3732a810d
anc1en7 <90032609+anc1en7@users.noreply.github.com>
2023-02-10 19:46:05 +0530

    Added button animation

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   function handleClick() {
     var buttonInnerHTML = this.innerHTML;
     makesound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   }
 }
 document.addEventListener("keydown", function (event) {
   makesound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makesound(key) {
@@ -43,4 +45,11 @@ function makesound(key) {
     default:
       break;
   }
+}
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
